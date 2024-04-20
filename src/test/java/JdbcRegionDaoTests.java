71cb815c7eb5ee6be871953606cfddfb1ed54ac9
Raina Vincelli <rainavincelli@gmail.com>
2024-04-19 23:27:52 -0400

    Submitted for mid module project

--- src/test/java/JdbcRegionDaoTests.java
+++ src/test/java/JdbcRegionDaoTests.java
@@ -1,9 +1,12 @@
 import org.junit.Before;
 import org.pokemon.Dao.JdbcRegionDao;
+import org.pokemon.Model.Region;
 
 public class JdbcRegionDaoTests extends BaseDaoTests{
 
     private JdbcRegionDao jdbcRegionDao;
+
+    private final Region REGION_1 = new Region();
 
     @Before
     private void setup(){
